b4bd13f72096858df17c3ff60d4418ea613d56e0
yqiuu <yishengq@student.unimelb.edu.au>
2018-08-28 14:44:47 +1000

    Rewrite the main function as a class
    -Rename density to prob

--- hdr.py
+++ hdr.py
@@ -5,41 +5,41 @@ import seaborn as sns
 from scipy.interpolate import InterpolatedUnivariateSpline as spl
 
 
-def get_hdr(density, q = 68):
-    inds = density > np.percentile(density, q = 100. - q)
+def get_hdr(prob, q = 68):
+    inds = prob > np.percentile(prob, q = 100. - q)
     if any(inds):
-        return inds, min(density[inds])
+        return inds, min(prob[inds])
     else:
-        maximum = max(density)
-        inds = density == maximum
+        maximum = max(prob)
+        inds = prob == maximum
         return inds, maximum
 
 
-def get_hdr_bounds(data, density, q = 68):
-    inds, p = get_hdr(density, q = q)
+def get_hdr_bounds(data, prob, q = 68):
+    inds, p = get_hdr(prob, q = q)
     hdr = data[inds]
     return min(hdr), max(hdr), p
 
 
-def hdr1d(data, density, norm = None, bins = 20, **kwargs):
+def plot_hdr1d(data, prob, norm = None, bins = 20, **kwargs):
     if np.isscalar(bins):
         bins = np.linspace(min(data), max(data), bins)
     elif type(bins) is dict:
         bins = bins[data.name]
     if norm is None:
-        density = density/max(density)
+        prob = prob/max(prob)
     else:
-        density = density/norm
+        prob = prob/norm
     xp = bins[:-1] + np.diff(bins)/2.
     yp = np.zeros(len(xp))
     for i, (l, u) in enumerate(zip(bins[:-1], bins[1:])):
-        p = density[(data >= l) & (data < u)]
+        p = prob[(data >= l) & (data < u)]
         yp[i] = max(p) if len(p) != 0 else 0.
     x = np.linspace(xp[0], xp[-1], 100)
     plt.plot(x, spl(xp, yp)(x), **kwargs)
 
 
-def hdr2d(xData, yData, density, regions = [68, 10], norm = None, logScale = False, **kwargs):
+def plot_hdr2d(xData, yData, prob, regions = [68, 10], norm = None, logScale = False, **kwargs):
     # Add default parameters
     if 's' not in kwargs:
         kwargs['s'] = 1
@@ -48,16 +48,16 @@ def hdr2d(xData, yData, density, regions = [68, 10], norm = None, logScale = Fal
         if key in kwargs:
             kwargs.pop(key)
     if norm is None:
-        density = density/max(density)
+        prob = prob/max(prob)
     else:
-        density = density/norm
+        prob = prob/norm
     #        
     if regions is None:
-        c = density
+        c = prob
     else:
-        c = np.full(len(density), -1.)
+        c = np.full(len(prob), -1.)
         for q in np.sort(regions)[::-1]:
-            inds, p = get_hdr(density, q)
+            inds, p = get_hdr(prob, q)
             c[inds] = p
     # Exclude all points beyond the largest region
     cond = c > 0.
@@ -66,7 +66,7 @@ def hdr2d(xData, yData, density, regions = [68, 10], norm = None, logScale = Fal
     c = np.array(c[cond])
     if logScale:
         c = np.log(c)
-    # Sort the data such that lower density regions are plotted first
+    # Sort the data such that lower prob regions are plotted first
     inds = np.argsort(c)
     x = x[inds]
     y = y[inds]
@@ -74,32 +74,32 @@ def hdr2d(xData, yData, density, regions = [68, 10], norm = None, logScale = Fal
     plt.scatter(x, y, c = c, **kwargs)
 
 
-def plot_hdr_bounds(xData, yData = None, density = None, regions = [68], **kwargs):
-    if density is None:
-        raise ValueError("density must be given!")
+def plot_hdr_bounds(xData, yData = None, prob = None, regions = [68], **kwargs):
+    if prob is None:
+        raise ValueError("prob must be given!")
     if 'linestyle' not in kwargs:
         kwargs['linestyle'] = '--'
     for q in regions:
-        lower, upper, _ = get_hdr_bounds(xData, density, q)
+        lower, upper, _ = get_hdr_bounds(xData, prob, q)
         plt.axvline(lower, **kwargs)
         if upper != lower:
             plt.axvline(upper, **kwargs)
     if yData is not None:
         for q in regions:
-            lower, upper, _ = get_hdr_bounds(yData, density, q)
+            lower, upper, _ = get_hdr_bounds(yData, prob, q)
             plt.axhline(lower, **kwargs)
             if upper != lower:
                 plt.axhline(upper, **kwargs)
 
 
-def plot_best_fit(xData, yData = None, density = None, best = None, kwargsDot = {}, **kwargs):
-    if density is not None:
-        idx, _ = get_hdr(density, q = 0.)
+def plot_best_fit(xData, yData = None, prob = None, best = None, kwargsDot = {}, **kwargs):
+    if prob is not None:
+        idx, _ = get_hdr(prob, q = 0.)
         bestX = np.unique(xData[idx])[0]
     elif best is not None:
         bestX = best[xData.name]
     else:
-        raise ValueError("Either density and best must be given!")
+        raise ValueError("Either prob and best must be given!")
     if 'linestyle' not in kwargs:
         kwargs['linestyle'] = '--'
     plt.axvline(bestX, **kwargs)
@@ -115,18 +115,18 @@ def plot_best_fit(xData, yData = None, density = None, best = None, kwargsDot = 
            plt.plot(bestX, bestY, **kwargsDot)
 
 
-def corner(data, density, upper = False, visible = False, cax = 'default',
+def corner(data, prob, upper = False, visible = False, cax = 'default',
            regions = [68, 10], norm = None, bins = 20, logScale = False,
            kwargs1d = {}, kwargs2d = {}, **kwargs):
     if type(data) is not pd.DataFrame:
         data = pd.DataFrame(data, columns = ['x%d'%i for i in range(data.shape[-1])])
     g = sns.PairGrid(data, **kwargs)
-    g.map_diag(hdr1d, density = density, norm = norm, bins = bins, **kwargs1d)
+    g.map_diag(hdr1d, prob = prob, norm = norm, bins = bins, **kwargs1d)
     if upper:
-        g.map_upper(hdr2d, density = density, regions = regions, norm = norm, logScale = logScale,
+        g.map_upper(hdr2d, prob = prob, regions = regions, norm = norm, logScale = logScale,
                     **kwargs2d)
     else:
-        g.map_lower(hdr2d, density = density, regions = regions, norm = norm, logScale = logScale,
+        g.map_lower(hdr2d, prob = prob, regions = regions, norm = norm, logScale = logScale,
                     **kwargs2d)
     ndim = len(g.axes)
     for iRow in range(ndim):
@@ -135,7 +135,7 @@ def corner(data, density, upper = False, visible = False, cax = 'default',
                 if norm is None:
                     maxY = 1.2
                 else:
-                    maxY = 1.2*max(density)/norm
+                    maxY = 1.2*max(prob)/norm
                 g.diag_axes[iRow].set_ylim(0., maxY)
             if visible:
                 continue
@@ -153,4 +153,46 @@ def corner(data, density, upper = False, visible = False, cax = 'default',
     plt.subplots_adjust(hspace = 0., wspace = 0.)
     return g
 
+
+class hdr_corner(sns.PairGrid):
+    def __init__(self, data, logProb, norm = None, **kwargs):
+        if type(data) is not pd.DataFrame:
+            data = pd.DataFrame(data, columns = ['x%d'%i for i in range(data.shape[-1])])
+        kwargs['diag_sharey'] = False
+        kwargs['despine']     = False
+        super().__init__(data, **kwargs)
+        #
+        prob = np.exp(logProb)
+        if norm is None:
+            prob = prob/max(prob)
+        else:
+            prob = prob/norm
+        self.prob = prob
+        #
+        self.map_diag(plot_hdr1d, prob = prob, norm = norm)
+        self.map_lower(plot_hdr2d, prob = prob, norm = norm)
+        yMax = 1.2*max(prob)
+        for iA, ax in enumerate(self.diag_axes):
+            ax.axis('on')
+            ax.set_xlabel('')
+            plt.setp(ax.get_xticklabels(), visible = False)
+            ax.set_ylim(0, yMax)
+            ax.set_yticks(np.linspace(0, yMax, 7)[1::2])
+            ax.yaxis.tick_right()
+            #
+            ax = self.axes[iA, iA]
+            ax.tick_params(axis = 'y', which = 'both', length = 0.)
+            if iA == 0:
+                plt.setp(ax.get_yticklabels(), visible = False)
+        #
+        ndim = len(self.axes)
+        for iRow in range(ndim):
+            for iCol in range(iRow + 1, ndim):
+                self.axes[iRow, iCol].axis('off')
+        plt.subplots_adjust(hspace = 0., wspace = 0.)
+
+
+    def _add_axis_labels(self):
+        # Disable the origianl class method
+        pass
 
