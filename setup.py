3cbde116df6ebc7eff5fab42bebdf82b41a5add4
yqiuu <yishengq@student.unimelb.edu.au>
2020-05-13 15:44:57 +1000

    Rename hdr as hdc

--- setup.py
+++ setup.py
@@ -1,11 +1,13 @@
 from setuptools import setup
 
-setup(name = 'hdr',
-      version = '0.1.0',
-      description = 'tool to visualise the highest density region (HDR) for MCMC samples',
-      url = 'https://github.com/yqiuu/hd-region',
-      author = 'Yisheng Qiu',
-      author_email = 'yishengq@student.unimelb.edu.au',
-      license = 'MIT',
-      packages = ['hdr'],
-      install_requires = ['numpy', 'scipy', 'matplotlib', 'pandas', 'seaborn'])
+setup(
+    name='hdc',
+    version='0.1.0',
+    description='tool to visualise the highest density region (HDR) for MCMC samples',
+    url='https://github.com/yqiuu/hd-region',
+    author='Yisheng Qiu',
+    author_email='yishengq@student.unimelb.edu.au',
+    license='MIT',
+    packages=['hdc'],
+    install_requires=['numpy', 'scipy', 'matplotlib', 'pandas', 'seaborn']
+)
